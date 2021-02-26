75652401d6e6c4d6f466a1e6833577dcc3a954c2
Dane Payba <72733141+dpayba@users.noreply.github.com>
2021-02-26 02:02:21 -1000

    StudentWorld Header Final

--- StudentWorld.h
+++ StudentWorld.h
@@ -8,11 +8,6 @@
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
-const double left_border = ROAD_CENTER-ROAD_WIDTH/2;
-const double left_white_line = ROAD_CENTER-ROAD_WIDTH/2+ROAD_WIDTH/3;
-const double right_white_line = ROAD_CENTER+ROAD_WIDTH/2-ROAD_WIDTH/3;
-const double right_border = ROAD_CENTER+ROAD_WIDTH/2;
-
 class StudentWorld : public GameWorld
 {
 public:
@@ -24,41 +19,35 @@ public:
     virtual void cleanUp();
     // Accessors
     GhostRacer* getPlayer() const;
-    int getSoulsToSave();
-    int getBonusPoints();
     int getPlayerX() const;
     int getPlayerY() const;
     double getPlayerRadius() const;
     int getPlayerDirection() const;
-    bool getLaneFound();
-    bool getActorNotFound();
     // Mutators
-    void setSoulsToSave(int num);
     void decreaseSoulsToSave();
-    void decreaseBonusPoints();
     void addWater(int x1, int y1);
-    void setLaneFound();
-    void resetLane();
-    void setActorNotFound();
-    void resetActor();
     void addHealingGoodie(int x1, int y1);
     void addOilSlick(int x1, int y1);
     void heal();
     void addSprays();
-    bool actorFront(int lane, int yPos);
+    bool actorFront(int lane, Actor* current);
+    bool actorBehind(int lane, Actor* current);
     bool overlapsWith(int x1, int y1, double r1, int x2, int y2, double r2) const;
     bool overlapsWithRacer(int x1, int y1, double radius) const;
     bool overlapsWithProjectile(int x1, int y1, double radius) const;
-    bool evalLeft();
-    bool evalMiddle();
-    bool evalRight();
 private:
     GhostRacer* m_player;
     std::list<Actor*> m_actors;
-    bool m_laneFound;
-    bool m_actorFound;
     int m_soulsToSave;
     int m_bonusPoints;
+    
+    int getSoulsToSave();
+    int getBonusPoints();
+    void setSoulsToSave(int num);
+    void decreaseBonusPoints();
+    bool evalLeft();
+    bool evalMiddle();
+    bool evalRight();
 };
 
 #endif // STUDENTWORLD_H_
